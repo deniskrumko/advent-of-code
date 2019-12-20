677fa567025dcc054fe1b51c11b1800e61035351
Denis Krumko <dkrumko@gmail.com>
2019-12-20 09:47:12 +0300

    Add day10/2019 - part 2

--- 2019/day10/main.py
+++ 2019/day10/main.py
@@ -12,6 +12,17 @@ class MonitoringStation:
         ]
         self.width = len(self._map[0])
         self.height = len(self._map)
+        self.history = []
+
+    def print_map(self):
+        print()
+
+        for line in self._map:
+            for value in line:
+                print(value, end='')
+            print()
+
+        input('')
 
     def get_best_position(self) -> tuple:
         """Get best position for monitoring station."""
@@ -67,6 +78,69 @@ class MonitoringStation:
 
         return not any_other_asteroids
 
+    def run_vaporisation(self, station: tuple):
+        distance = max(self.width, self.height)
+        total_counter = 0
+
+        self._map[station[1]][station[0]] = '@'
+
+        while True:
+            found_asteroids = False
+            for jump in self.get_turning_jumps(distance):
+                current = station
+                while True:
+                    current = (current[0] + jump[0], current[1] - jump[1])
+                    if current[0] < 0 or current[1] < 0:
+                        break
+
+                    try:
+                        value = self._map[current[1]][current[0]]
+                    except IndexError:
+                        break
+
+                    if value == '#':
+                        found_asteroids = True
+                        total_counter += 1
+                        self._map[current[1]][current[0]] = total_counter
+                        self.history.append(current)
+                        break
+
+            if not found_asteroids:
+                break
+
+        return total_counter
+
+    @classmethod
+    def get_all_moves(cls, distance):
+        for i in range(distance + 1):
+            for j in range(distance + 1):
+                yield (i, j)
+
+    @classmethod
+    def get_turning_jumps(cls, distance):
+        x = list(cls.get_all_moves(distance))
+        res = set()
+        for i in x:
+            gcd = math.gcd(*i)
+            if gcd == 0:
+                continue
+            res.add((i[0] // gcd, i[1] // gcd))
+
+        def myfunc(coord):
+            if coord == (1, 0):
+                return 0
+            if coord == (0, 1):
+                return 90
+            return math.degrees(math.atan(coord[1]/coord[0]))
+
+        p1 = list(reversed(sorted(list(res), key=myfunc)))
+        p2 = [(i, -j) for i, j in p1]
+        p2[0], p2[-1] = p2[-1], p2[0]
+        p3 = [(-i, -j) for i, j in p1]
+        p4 = [(-j, i) for i, j in p1]
+        result = p1 + p2[1:] + p3[1:] + p4[1:-1]
+        return result
+
 
 if __name__ == '__main__':
     input_file = '2019/day10/input.txt'
@@ -80,3 +154,10 @@ if __name__ == '__main__':
             f'Best position for monitoring station is {coordinates}'
             f' with {count} asteroids'
         )
+
+        ms.run_vaporisation(station=coordinates)
+        asteroid_200 = ms.history[199]
+        print(f'Asteroid #200 that was vaporised is {asteroid_200}')
+
+        code = asteroid_200[0] * 100 + asteroid_200[1]
+        print(f'Secret code: {code}')  # 1707
--- 2019/day10/test_main.py
+++ 2019/day10/test_main.py
@@ -1,75 +1,13 @@
 import pytest
+
 from .main import MonitoringStation
-
-map_1 = """
-.#..#
-.....
-#####
-....#
-...##
-"""
-
-map_2 = """
-......#.#.
-#..#.#....
-..#######.
-.#.#.###..
-.#..#.....
-..#....#.#
-#..#....#.
-.##.#..###
-##...#..#.
-.#....####
-"""
-
-map_3 = """
-#.#...#.#.
-.###....#.
-.#....#...
-##.#.#.#.#
-....#.#.#.
-.##..###.#
-..#...##..
-..##....##
-......#...
-.####.###.
-"""
-
-map_4 = """
-.#..#..###
-####.###.#
-....###.#.
-..###.##.#
-##.##.#.#.
-....###..#
-..#.#..#.#
-#..#.#.###
-.##...##.#
-.....#.#..
-"""
-
-map_5 = """
-.#..##.###...#######
-##.############..##.
-.#.######.########.#
-.###.#######.####.#.
-#####.##.#.##.###.##
-..#####..#.#########
-####################
-#.####....###.#.#.##
-##.#################
-#####.##.###..####..
-..######..##.#######
-####.##.####...##..#
-.#####..#.######.###
-##...#.##########...
-#.##########.#######
-.####.#.###.###.#.##
-....##.##.###..#####
-.#.#.###########.###
-#.#.#.#####.####.###
-###.##.####.##.#..##
-"""
+from .maps import (
+    map_1,
+    map_2,
+    map_3,
+    map_4,
+    map_5,
+)
 
 
 @pytest.mark.parametrize('space_map,x,y,count', (
@@ -80,6 +18,7 @@ map_5 = """
     (map_5, 11, 13, 210),
 ))
 def test_get_amount_of_visible_asteroids(space_map, x, y, count):
+    """Test `get_amount_of_visible_asteroids`."""
     ms = MonitoringStation(space_map.strip())
     assert ms.get_amount_of_visible_asteroids(x, y) == count
 
@@ -92,7 +31,48 @@ def test_get_amount_of_visible_asteroids(space_map, x, y, count):
     (map_5, 11, 13, 210),
 ))
 def test_get_best_position(space_map, x, y, expected_count):
+    """Test `get_best_position.`"""
     ms = MonitoringStation(space_map.strip())
     coordinates, asteroids = ms.get_best_position()
     assert coordinates == (x, y)
     assert asteroids == expected_count
+
+
+@pytest.mark.parametrize('distance,expected', (
+    (1, 8 * 1),
+    (2, 8 * 2),
+    (3, 8 * 4),
+    (4, 8 * 6),
+    (5, 8 * 10),
+))
+def test_get_turning_jumps(distance, expected):
+    """Test `get_turning_jumps`."""
+    jumps = list(MonitoringStation.get_turning_jumps(distance))
+    assert len(jumps) == expected
+
+
+def test_run_vaporisation():
+    """Test `run_vaporisation`."""
+    ms = MonitoringStation(map_1.strip())
+    result = ms.run_vaporisation(station=(3, 4))
+    assert result == 9
+
+
+@pytest.mark.parametrize('order,coordinates', (
+    (1, (11, 12)),
+    (2, (12, 1)),
+    (3, (12, 2)),
+    (10, (12, 8)),
+    (20, (16, 0)),
+    (50, (16, 9)),
+    (100, (10, 16)),
+    (199, (9, 6)),
+    (200, (8, 2)),
+    (201, (10, 9)),
+    (299, (11, 1)),
+))
+def test_map_5_vaporisation(order, coordinates):
+    """Test vaporisation order on map #5."""
+    ms = MonitoringStation(map_5.strip())
+    ms.run_vaporisation(station=(11, 13))
+    assert ms.history[order - 1] == coordinates

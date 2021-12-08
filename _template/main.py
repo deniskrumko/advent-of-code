ad8ebc88d21276579c2981819ffcf98ce46fa241
Denis Krumko <dkrumko@gmail.com>
2021-12-08 23:53:13 +0300

    Add template

---
+++ _template/main.py
@@ -0,0 +1,9 @@
+def function(data: list):
+    ...
+
+
+if __name__ == '__main__':
+    with open('2021/day_XX/input.txt', 'r') as f:
+        input_data = [int(i) for i in f.readlines()]
+        print(f'Your result (1): {function(input_data)}')
+        print(f'Your result (2): {function(input_data)}')
---
+++ _template/test_main.py
@@ -0,0 +1,7 @@
+from .main import function
+
+input_data = []
+
+
+def test_function():
+    assert not function(input_data)

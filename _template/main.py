f513ce6ba6de1a6d1710946459697057ba992eeb
Denis Krumko <dkrumko@gmail.com>
2021-12-09 00:51:53 +0300

    Update template

--- _template/main.py
+++ _template/main.py
@@ -1,5 +1,5 @@
 def function(data: list):
-    ...
+    return True
 
 
 if __name__ == '__main__':
--- _template/test_main.py
+++ _template/test_main.py
@@ -1,7 +1,27 @@
+import pytest
+
 from .main import function
 
 input_data = []
 
 
-def test_function():
-    assert not function(input_data)
+@pytest.fixture
+def file_input_data():
+    with open('2021/day_XX/input.txt', 'r') as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def test_function_1():
+    assert function(input_data)
+
+
+def test_function_1_from_file(file_input_data):
+    assert function(file_input_data)
+
+
+def test_function_2():
+    assert function(input_data)
+
+
+def test_function_2_from_file(file_input_data):
+    assert function(file_input_data)

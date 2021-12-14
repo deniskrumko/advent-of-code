c6b1303cbb3a2e97264dc72d5c9b178ea9222453
Denis Krumko <dkrumko@gmail.com>
2021-12-14 14:00:09 +0300

    Update template

--- _template/main.py
+++ _template/main.py
@@ -4,6 +4,6 @@ def function(data: list):
 
 if __name__ == '__main__':
     with open('2021/day_XX/input.txt', 'r') as f:
-        input_data = [int(i) for i in f.readlines()]
+        input_data = [line.strip() for line in f.readlines()]
         print(f'Your result (1): {function(input_data)}')
         print(f'Your result (2): {function(input_data)}')
